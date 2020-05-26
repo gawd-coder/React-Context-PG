8fdf42911af8b1f0c727ee5543565aa48b2a3cb7
gawd-coder <ansari.ammar786@gmail.com>
2020-05-26 07:21:47 +0530

    Accessing the context within components

--- my-app/src/components/BookList.js
+++ my-app/src/components/BookList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import {ThemeContext} from "../contexts/ThemeContext"
 
 export default class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme  = isLightTheme ? light : dark;
         return (
-            <div className = "book-list">
+            <div className = "book-list" style = {{background : theme.bg, color: theme.syntax}}>
                 <ul>
-                    <li>Book 01</li>
-                    <li>Book 02</li>
-                    <li>Book 03</li>
-                    <li>Book 04</li>
+                    <li style = {{background : theme.ui}}>Book 01</li>
+                    <li style = {{background : theme.ui}}>Book 02</li>
+                    <li style = {{background : theme.ui}}>Book 03</li>
+                    <li style = {{background : theme.ui}}>Book 04</li>
                 </ul>
             </div>
         )
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
-
+    import {ThemeContext} from "../contexts/ThemeContext"
 export default class Navbar extends Component {
+    //this is used to provide the data (...state) from Context provider down to the components
+    static contextType = ThemeContext;
     render() {
+        console.log(this.context);
+        const { isLightTheme, light, dark } = this.context;
+        const theme  = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style = {{background : theme.ui, color: theme.syntax}}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
